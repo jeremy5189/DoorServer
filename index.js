34f97ab85203a6c2be8c2c5ab67813e562f9e27d
Jeremy Yen (Rpi) <jeremy5189@gmail.com>
2016-09-09 15:27:46 +0800

    Add init

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+// Init GPIO
+run_cmd('./scripts/init.sh', [], function(){});
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
